d308d57d0b91fdd7ea028a60421731a0620ed055
arishuynhvan <arishuynhvan@gmail.com>
2020-05-21 15:37:53 +0700

    Simple gameoverscreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-const GameOverScreen = props => {
-    return (
-        <View>
-
-        </View>
-    );
+const GameOverScreen = (props) => {
+  return (
+    <View>
+      <Text>The game is over!</Text>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default GameOverScreen;
-
